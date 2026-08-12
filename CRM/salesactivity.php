05d7e2855d906af003da2d29939619b1fcf969cf
PTGlobalMediaCreative <ptglobalmediacreative@gmail.com>
2026-08-12 13:42:03 +0700

    Commit

--- CRM/salesactivity.php
+++ CRM/salesactivity.php
@@ -1780,12 +1780,6 @@ if (isset($_GET['complete'])) {
                     <i class="fas fa-bars"></i>
                 </button>
                 <div>
-                    <nav aria-label="breadcrumb">
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="dashboard.php"><i class="fas fa-home"></i> Home</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">Sales Activity</li>
-                        </ol>
-                    </nav>
                     <h4><span><i class="fas fa-chart-bar" style="color:#ffd700;"></i></span> Sales Activity</h4>
                 </div>
             </div>
