c880267321d86c1908c0cc2d6b70ce514984f2fa
PTGlobalMediaCreative <ptglobalmediacreative@gmail.com>
2026-09-14 16:01:11 +0700

    Commit

--- CRM/navigation.php
+++ CRM/navigation.php
@@ -40,7 +40,7 @@ $showMenu = static function($name) use ($menuNames) {
     <?php endif; ?>
     <?php if ($showMenu('account_management')): ?><a class="<?= $currentPage === 'account_management.php' ? 'active' : '' ?>" href="account_management.php"><i class="fas fa-building"></i><span>Account Management</span></a><?php endif; ?>
     <?php if ($showMenu('transaction_request')): ?><a class="<?= in_array($currentPage, ['transactionrequest.php', 'detailtr.php'], true) ? 'active' : '' ?>" href="transactionrequest.php"><i class="fas fa-file-signature"></i><span>Transaction Request</span></a><?php endif; ?>
-    <?php if ($showMenu('produk')): ?><a class="<?= $currentPage === 'produk.php' ? 'active' : '' ?>" href="produk.php"><i class="fas fa-box"></i><span>Produk</span></a><?php endif; ?>
+    <?php if ($showMenu('produk')): ?><a class="<?= $currentPage === 'produk.php' ? 'active' : '' ?>" href="produk.php"><i class="fas fa-truck"></i><span>Produk</span></a><?php endif; ?>
     <?php if ($showMenu('delivery_order')): ?><a class="<?= in_array($currentPage, ['deliveryinstruction.php', 'detaildi.php'], true) ? 'active' : '' ?>" href="deliveryinstruction.php"><i class="fas fa-file-signature"></i><span>Delivery Instruction</span></a><?php endif; ?>
     <div class="rail-label">Administration</div>
     <?php if ($showMenu('data_user')): ?><a class="<?= $currentPage === 'data_user.php' ? 'active' : '' ?>" href="data_user.php"><i class="fas fa-users"></i><span>Data User</span></a><?php endif; ?>
