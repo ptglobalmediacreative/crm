363b1e942b7e448e747036c5c23882fc8c90b505
PTGlobalMediaCreative <ptglobalmediacreative@gmail.com>
2026-09-14 15:44:02 +0700

    Commit

--- CRM/deliveryinstruction.php
+++ CRM/deliveryinstruction.php
@@ -199,6 +199,7 @@ $totalDeliveries = $totalPending + $totalApproved + $totalRejected;
     
     <link rel="icon" type="image/webp" href="images/favicon.webp">
     <link rel="shortcut icon" type="image/webp" href="images/favicon.webp">
+    <link rel="stylesheet" href="css/navigation.css">
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
