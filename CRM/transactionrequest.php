01d464561db316060085920fbccf81dadde24906
PTGlobalMediaCreative <ptglobalmediacreative@gmail.com>
2026-09-18 12:52:17 +0700

    Commit

--- CRM/transactionrequest.php
+++ CRM/transactionrequest.php
@@ -362,7 +362,7 @@ $totalRequests = $totalPending + $totalApproved + $totalRejected;
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/navigation.css">
     <link rel="stylesheet" href="css/transactionrequest.css">
-    <link rel="stylesheet" href="css/notifications.css">
+    <link rel="stylesheet" href="css/notification.css">
 </head>
 <body>
 
