d11773e6d460b716757783c5cea5c0bc17d061ac
PTGlobalMediaCreative <ptglobalmediacreative@gmail.com>
2026-09-18 14:28:09 +0700

    Commit

--- CRM/transactionrequest.php
+++ CRM/transactionrequest.php
@@ -360,9 +360,9 @@ $totalRequests = $totalPending + $totalApproved + $totalRejected;
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/transactionrequest.css">
+    <link rel="stylesheet" href="css/transactionrequest.css?v=20260918-2">
 </head>
-<body>
+<body class="page-transactionrequest">
 
     <?php require_once 'navigation.php'; ?>
 
@@ -467,7 +467,7 @@ $totalRequests = $totalPending + $totalApproved + $totalRejected;
                                 <?php foreach ($allowedNextApprovers as $approver): ?>
                                     <option
                                         value="<?= htmlspecialchars($approver) ?>"
-                                        <?= $next_approver_filter === $appro_approver ? 'selected' : '' ?>>
+                                        <?= $next_approver_filter === $approver ? 'selected' : '' ?>>
                                         <?= htmlspecialchars($approver) ?>
                                     </option>
                                 <?php endforeach; ?>
